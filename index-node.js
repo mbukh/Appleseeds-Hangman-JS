2c1368fc207f2a0721a9c68acfb863110844d9f0
mbukhman <mbukhman@gmail.com>
2022-10-03 01:59:47 +0400

    Node JS version update 	modified:   index-node.js 	modified:   package-lock.json 	modified:   package.json

--- index-node.js
+++ index-node.js
@@ -1,187 +1,253 @@
-// Node.js modules import (CLI Mode)
-// /*
-import figlet from 'figlet';
-import fetch from 'node-fetch';
-import promptSync from 'prompt-sync';
-const prompt = promptSync();
-// */
-// end Node.js import (CLI Mode)
+#!/usr/bin/env node
+"use strict";
 
-// js-beautifier: https://jsonformatter.org/jsbeautifier
+const figlet = require("figlet");
+const https = require('node:https');
+const readline = require('node:readline');
 
-(function () {
-    "use strict";
+const wordsOffline = ["absolute", "accomplishment", "affection", "artistry",
+    "beautiful", "bonus", "calm", "character", "cheerful", "comedy",
+    "companionship", "compassion", "compassionate", "completion",
+    "consciousness", "courageous", "delight", "dressy", "enjoyment",
+    "entertaining", "entrepreneur", "excellence", "excited",
+    "exciting", "expressive", "faithful", "fantastic", "free", "freedom",
+    "friendly", "fun", "genius", "genuine", "giggle", "good", "goodness",
+    "happiness", "happy", "heavenly", "honest", "hug", "immaculate",
+    "incredible", "independent", "jolly", "joy", "joyful", "kind",
+    "knowledgeable", "language", "laugh", "laughter", "likable", "live",
+    "lovable", "love", "lovemaking", "lover", "magical", "meditation",
+    "oasis", "orgasm", "outsmart", "ownership", "paradise", "particular",
+    "payday", "peaceful", "pizza", "polite", "prize", "project", "rejoice",
+    "relaxation", "relaxing", "responsible",
+    "sanitary", "shower", "sincerity", "skill", "skilled", "smile",
+    "strength", "style", "successful", "sunny", "sunshine", "talented",
+    "thinker", "thriving", "tranquility", "triumph", "upbeat", "vacation",
+    "win", "winner", "wisdom"
+];
 
-    const isBrowser = (typeof window !== "undefined");
+let word = wordsOffline[Math.floor(Math.random() * wordsOffline.length)];
 
-    const wordsOffline = ["program", "disaster", "macaque", "beautiful", "dinosaur",
-        "computer", "fritters", "index",
-        "school", "introvert", "helicopter", "institute", "nature", "alphabet",
-        "willow", "hamadryl", "curator", "field",
-        "performer", "customer", "alternative", "quotes", "string", "livestock",
-        "absolute"
-    ];
 
-    let word = wordsOffline[Math.floor(Math.random() * wordsOffline.length)];
+async function fetchWord() {
+    // Fetch words from API
+    const apiUrl = "https://random-word-api.herokuapp.com/word";
+    // Async fetch
+    return await fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => (typeof data[0] === "string" ? (word = data[0]) : null))
+        .catch((error) => msgError("Fetch Error. Starting an offline game."));
+    // .finally(() => startGame());
+}
 
-    async function fetchWord() {
-        // Fetch words from API
-        const apiUrl = "https://random-word-api.herokuapp.com/word";
-        // Async fetch
-        return fetch(apiUrl)
-            .then((response) => response.json())
-            .then((data) => (typeof data[0] === "string" ? (word = data[0]) : null))
-            .catch((error) => console.log("%cFetch Error. Starting an offline game.", "color: #C66"));
-        // .finally(() => startGame());
+
+async function showWelcomeScreen() {
+    console.clear();
+    const msg = "HANG MAN";
+
+    if (typeof figlet === "undefined") {
+        msgError("Figlet module not found.");
+        msgLog(msg, "color: #E44; font-size: 2em; text-shadow: 1px 1px #000;");
+        return;
     }
 
+    // Use figlet module
+    figlet(msg,
+        { font: "Patorjk's Cheese" },
+        (err, text) => {
+            msgLog(text, "color: #CC3333; font-size:10px; text-shadow: 2px 2px #000;");
+        });
+    // Help on slow connections
+    await sleep(500);
+}
 
+
+
+async function startGame() {
     showWelcomeScreen();
-    startGame();
+    await fetchWord();
 
+    let answerArray = new Array(word.length).fill("*");
+    let guessHistorySet = new Set();
+    let remainingLetters = word.length;
+    let attempts = 10;
+    let isHit = false; // success status
+    let isRepeat = false; // repeat status
+    let guess = "";
 
-    function showWelcomeScreen() {
-        if (isBrowser) {
-            if (window.location.protocol === "file:") {
-                alert("fetch APi does not support file: protocol.");
-            }
-            figlet.defaults({
-                fontPath: "./node_modules/figlet/fonts"
-            });
-            figlet.preloadFonts(["Patorjk's Cheese"], function () {
-                // console.log("prefetching done (only did it for 2 fonts)!");
-            });
-        }
-
-        console.clear();
-        const msg = "HANG MAN";
-        figlet(msg,
-            { font: "Patorjk's Cheese" },
-            (err, text) => {
-                console.log("%c" + text, "color: #CC3333; font-size:10px; text-shadow: 2px 2px #000;");
-            });
-    }
-
-
-    async function startGame() {
-        await fetchWord();
-
-        let answerArray = new Array(word.length).fill("*");
-        let guessHistorySet = new Set();
-
-        let remainingLetters = word.length;
-        let attempts = 10;
-        let isHit = false; // success status
-        let isRepeat = false; // repeat status
-
-        let guess = "";
-
-        console.log(`We've got a word for you. It consists of ${word.length} letters.
+    msgLog(`We've got a word for you. It consists of ${word.length} letters.
 Guess the the entire word or try letter by letter.
 You have 10 attempts. Good luck!`);
-        console.log("Hint: %c" + word, "background: #888; color: #888");
+    msgLog("Hint: " + word, "background: #888; color: #888");
 
-        while ((remainingLetters > 0) && (attempts > 0)) {
-            if (isHit && !isRepeat) {
-                // A new guess was done before
-                console.log("%cThat is correct.", "color: #4B4");
-            }
+    while ((remainingLetters > 0) && (attempts > 0)) {
+        if (isHit && !isRepeat) {
+            // A new guess was done before
+            msgSuccess("That is correct.");
+        }
 
-            // Print guessed letters
-            console.log(answerArray.join("  "));
+        // Print guessed letters
+        msgLog(answerArray.join("  "));
 
-            // Ask for a new guess
-            if (isBrowser) {
-                // Browser only
-                console.log("Guess a letter or the entire word: ");
-                guess = prompt("Guess a letter or the entire word:\n(Cancel to quit the game)");
-                console.log("> " + (guess ? guess : "let me out of here."));
+        // Ask for a new guess
+        guess = await prompt("Guess a letter or the entire word: ");
+        msgLog(">" + guess);
+
+        // Fix an empty input
+        if (guess === "") {
+            guess = " ";
+        }
+
+        guess = guess.toLowerCase();
+
+        // Reset success status
+        isHit = false;
+        // Check the guess history
+        isRepeat = guessHistorySet.has(guess);
+
+        // Invalid or empty input
+        if (!(/^[a-zA-Z]+$/).test(guess)) {
+            msgWarning("Wrong input. Try again...");
+        }
+        // Full word correct guess
+        else if (guess === word) {
+            answerArray = guess.split("");
+            break;
+        }
+        // Repeating a previous guess
+        else if (isRepeat) {
+            if (word.indexOf(guess) === -1) {
+                // Not in the word
+                msgWarning("Remember the past. There's no \"" + guess + "\".");
             } else {
-                // CLI (node.js)
-                guess = prompt("Guess a letter or the entire word: ");
-            }
-
-            if (guess === null) {
-                // Cancel button returns null
-                // Exit the game
-                endGame(0);
-                return;
-            } else if (guess === "") {
-                // Fix an empty input
-                guess = " ";
-            }
-
-            guess = guess.toLowerCase();
-
-            // Reset success status
-            isHit = false;
-            isRepeat = guessHistorySet.has(guess);
-
-            if (!(/^[a-zA-Z]+$/).test(guess)) {
-                // Invalid or empty input
-                console.log("%cWrong input. Try again...", "color: #e89417");
-            } else if (guess === word) {
-                // Full word correct guess
-                answerArray = guess.split("");
-                break;
-            } else if (isRepeat) {
-                // Repeating a previous guess
-                if (word.indexOf(guess) === -1) {
-                    // Not in the word
-                    console.log("%cRemember the past. There's no \"" + guess + "\".", "color: #e89417");
-                } else {
-                    // Inside the word
-                    console.log("%cYou've found all \"" + guess + "\".", "color: #e89417");
-                }
-            } else {
-                // Check a Valid New guess in the word
-                for (let j = 0; j < word.length; j++) {
-                    if (word[j] === guess) {
-                        answerArray[j] = guess;
-                        remainingLetters--;
-                        isHit = true;
-                    }
-                }
-                if (!isHit && !isRepeat) {
-                    // Lose a life
-                    attempts--;
-                    if (attempts === 0) {
-                        // Game over break the game
-                        console.log("%c0 attempts left.", "color: #C66");
-                        break;
-                    }
-                    if (guess.length > 1) {
-                        // A word guess (probably)
-                        console.log("%cHaste makes waste.", "color: #C66");
-                    }
-                    // Show attempts count
-                    console.log("%cYou have " + attempts + ((attempts > 1) ?
-                        " attempts" : " attempt") + " left.", "color: #C66");
-                }
-            }
-            // Log a new guess (one letter guesses are logged)
-            if (/^[a-zA-Z]$/.test(guess)) {
-                guessHistorySet.add(guess);
+                // Inside the word
+                msgWarning("You've found all \"" + guess + "\".");
             }
         }
-        endGame(attempts);
-    }
-
-    function endGame(attempts) {
-        if (attempts === 0) {
-            // Show lose message
-            console.log(`
-			Game over!
-	  The word was "${word}".
-	  `);
-        } else {
-            // Show win message
-            console.log("%c" + word.split("").join("  "), "color: #4B4");
-            console.log(`
-			  Victory!
-	You guessed the word "${word}".
-	`);
+        // Check a Valid New guess in the word
+        else {
+            for (let j = 0; j < word.length; j++) {
+                if (word[j] === guess) {
+                    answerArray[j] = guess;
+                    remainingLetters--;
+                    isHit = true;
+                }
+            }
+            if (!isHit && !isRepeat) {
+                // Lose a life
+                attempts--;
+                if (attempts === 0) {
+                    // Game over break the game
+                    msgError("0 attempts left.");
+                    break;
+                }
+                if (guess.length > 1) {
+                    // A word guess (probably)
+                    msgError("Haste makes waste.");
+                }
+                // Show attempts count
+                msgError("You have " + attempts +
+                    ((attempts > 1) ? " attempts" : " attempt") +
+                    " left.");
+            }
+        }
+        // Log a new guess (one letter guesses are logged)
+        if (/^[a-zA-Z]$/.test(guess)) {
+            guessHistorySet.add(guess);
         }
     }
+    endGame(attempts);
+}
 
-})();
+
+function endGame(attempts) {
+    if (attempts === 0) {
+        // Show lose message
+        msgError(`
+          Game over!
+    The word was "${word}".
+  `);
+    } else {
+        msgSuccess(word.split("").join("  "));
+        // Show win message
+        msgSuccess(`
+              Victory!
+    You guessed the word "${word}".
+`);
+    }
+}
+
+
+const msgLog = (text, style) => {
+    console.log("%c" + text, (style ? style : "font-size: 1.3em;"));
+};
+const msgError = (text) => console.log("%c" + text, "font-size: 1.3em; color: #C66");
+const msgSuccess = (text) => console.log("%c" + text, "font-size: 1.3em; color: #4B4");
+const msgWarning = (text) => console.log("%c" + text, "font-size: 1.3em; color: #e89417");
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+
+
+function prompt(q) {
+    return new Promise(resolve => {
+        let rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.question(q, a => {
+            resolve(a);
+            rl.close();
+        });
+    });
+}
+
+
+
+function fetch(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            var { statusCode } = res;
+            var contentType = res.headers['content-type'];
+            let error;
+            if (statusCode !== 200) {
+                error = new Error('Request Failed.\n' +
+                    `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error('Invalid content-type.\n' +
+                    `Expected application/json but received ${contentType}`);
+            }
+            if (error) {
+                console.error(error.message);
+                // consume response data to free up memory
+                res.resume();
+            }
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => {
+                rawData += chunk;
+            });
+            res.on('end', () => {
+                try {
+                    let obj = {
+                        data: '',
+                        json() {
+                            return JSON.parse(this.data);
+                        }
+                    };
+                    // const parsedData = JSON.parse(rawData);
+                    obj.data = rawData;
+                    resolve(obj);
+                } catch (e) {
+                    reject(e.message);
+                }
+            });
+        }).on('error', (e) => {
+            reject(`Got error: ${e.message}`);
+        });
+
+    });
+}
+
+
+
+
+startGame();
